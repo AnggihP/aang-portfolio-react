49b28b213f4bbcace65b54a9381ac8a20665cef7
Anggih Pratama <anggih10@gmail.com>
2023-08-31 14:59:24 +0800

    fixing error on image

--- src/data/project-all-data.ts
+++ src/data/project-all-data.ts
@@ -1,4 +1,14 @@
 import img24 from "/src/assets/projects/w24-group4.jpg";
+import img22 from "/src/assets/projects/w22-groupd.jpg";
+import img19 from "/src/assets/projects/w19list.jpg";
+import img18 from "/src/assets/projects/w18.jpg";
+import img17 from "/src/assets/projects/w17-backend-groupd.jpg";
+import img0 from "/src/assets/projects/nophoto.jpg";
+import img10 from "/src/assets/projects/w10.jpg";
+import img9 from "/src/assets/projects/w9.jpg";
+import img5 from "/src/assets/projects/w5.jpg";
+import img4 from "/src/assets/projects/w4.jpg";
+import img3 from "/src/assets/projects/w3.jpg";
 
 export const projects = [
     {
@@ -13,28 +23,28 @@ export const projects = [
       title: "Group Project - NaNInn",
       description: "Next.js, Tailwind, Typescript, Prisma, GoogleAuth, NextAuth, PostgreSQL ",
       link: "https://github.com/AnggihP/w22-group-react-group-d",
-      img: "../src/assets/projects/w22-groupd.jpg",
+      img: img22,
     },
     {
       id: 3,
       title: "React Features",
       description: "React, Vite, Tailwind, Postman, Typescript",
       link: "https://github.com/AnggihP/w19-react-features-AnggihP",
-      img: "../src/assets/projects/w19list.jpg",
+      img: img19,
     },
     {
       id: 4,
       title: "Simple Web Page featuring React",
       description: "React, Vite, Tailwind, Typescript",
       link: "https://github.com/AnggihP/w18-react-page-AnggihP",
-      img: "../src/assets/projects/w18.jpg",
+      img: img18,
     },
     {
       id: 5,
       title: "Our Backend - BookmarX",
       description: "Dockerfile, Cloud, PostgreSQL, Typescript, Cloudflare",
       link: "https://github.com/AnggihP/w17-our-backend-group-d",
-      img: "../src/assets/projects/w17-backend-groupd.jpg",
+      img: img17,
     },
 
     {
@@ -42,54 +52,54 @@ export const projects = [
       title: "Movie Watchlist Site",
       description: "HTML, Tailwind CSS, Javascript, RestAPI, Typescript",
       link: "https://github.com/AnggihP/w11-the-movie-AnggihP",
-      img: "../src/assets/projects/nophoto.jpg",
+      img: img0,
     },
     {
       id: 7,
       title: "MockAPI CRUD using Local Storage",
       description: "HTML, Tailwind CSS, Javascript, RestAPI",
       link: "https://github.com/AnggihP/w10-my-web-api-AnggihP",
-      img: "../src/assets/projects/w10.jpg",
+      img: img10,
     },
     {
       id: 8,
       title: "Library Tailwind",
       description: "HTML,Tailwind CSS, Javascript, Dockerfile",
       link: "https://github.com/AnggihP/w9-library-tailwind-AnggihP",
-      img: "../src/assets/projects/w9.jpg",
+      img: img9,
     },
     {
       id: 9,
       title: "Developer Survey CLI",
       description: "Javascript",
       link: "https://github.com/AnggihP/w7-developer-survey-cli-AnggihP",
-      img: "../src/assets/projects/nophoto.jpg",
+      img: img0,
     },
     {
       id: 10,
       title: "E-Commerce Design",
       description: "HTML",
       link: "https://github.com/AnggihP/w6-e-commerce-design-AnggihP",
-      img: "../src/assets/projects/nophoto.jpg",
+      img: img0,
     },
     {
       id: 11,
       title: "Company Website - Lamborghini Company",
       description: "HTML, CSS, Cloudflare, Wireframe, MockUp",
       link: "https://github.com/AnggihP/w5-company-website-group-d",
-      img: "../src/assets/projects/w5.jpg",
+      img: img5,
     },    {
       id: 12,
       title: "Deployment",
       description: "HTML, CSS, Cloudflare",
       link: "https://github.com/AnggihP/w4-my-portfolio-AnggihP",
-      img: "../src/assets/projects/w4.jpg",
+      img: img4,
     },
     {
       id: 13,
       title: "My Landing Website - Man City Fanbase",
       description: "HTML, CSS",
       link: "https://github.com/AnggihP/w3-landing-website-AnggihP",
-      img: "../src/assets/projects/w3.jpg",
+      img: img3,
     },
   ];
